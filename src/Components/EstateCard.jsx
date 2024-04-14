58030a06042dc4bd9a732cab05980947ddce23fd
RakibHossen678 <rh491464@gmail.com>
2024-04-14 12:33:41 +0600

    showed json data on UI

--- src/Components/EstateCard.jsx
+++ src/Components/EstateCard.jsx
@@ -11,11 +11,13 @@ const EstateCard = ({ estate }) => {
     area,
     location,
     segment_name,
-    id
+    id,
   } = estate;
   return (
     <div className="indicator">
-      <span className="indicator-item badge bg-[#00BCD4] px-4 py-3 text-white">{status}</span>
+      <span className="indicator-item badge bg-[#00BCD4] px-4 py-3 text-white">
+        {status}
+      </span>
       <div className="card w-96 bg-base-100 shadow-xl">
         <figure>
           <img src={image} alt="Shoes" />
@@ -23,8 +25,28 @@ const EstateCard = ({ estate }) => {
         <div className="px-3 py-2">
           <h2 className="card-title mb-1">{estate_title}</h2>
           <p>{description}</p>
-          <div className="card-actions">
-            <button className=" px-3 py-2 rounded-lg text-white bg-[#00BCD4] my-3"><Link to={`/estate/${id}`}>View Property</Link> </button>
+          <div className="pt-2 flex items-center space-x-2">
+            <h1 className="font-medium">Facilites:</h1>
+            {facilities.map((facility, idx) => (
+              <span key={idx} className="">
+                {facility},
+              </span>
+            ))}
+          </div>
+          <div className="">
+            <div className="flex justify-between py-2">
+              <p>{price}</p>
+              <p>{segment_name}</p>
+            </div>
+            <div className="flex justify-between py-2">
+              <p>{area}</p>
+              <p>{location}</p>
+            </div>
+          </div>
+          <div className="card-actions justify-center">
+            <button className=" px-3 py-2 rounded-lg text-white bg-[#00BCD4] my-3 flex ">
+              <Link to={`/estate/${id}`}>View Property</Link>{" "}
+            </button>
           </div>
         </div>
       </div>
