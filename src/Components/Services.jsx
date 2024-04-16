32cddbc430e643cebe585efc5d807e1546deda51
RakibHossen678 <rh491464@gmail.com>
2024-04-16 16:54:28 +0600

    added services  section

---
+++ src/Components/Services.jsx
@@ -0,0 +1,22 @@
+const Services = ({ service }) => {
+  console.log(service);
+  const { description, img, title } = service;
+  return (
+    <div className="max-w-xs p-3 rounded-md shadow-xl">
+      <div className="flex justify-center items-center">
+        <img src={img} alt="" className="w-28 rounded-md " />
+      </div>
+      <div className="mt-6 mb-2">
+        <h2 className="text-xl font-semibold tracking-wide">{title}</h2>
+      </div>
+      <p className="">{description}</p>
+      <div className="flex justify-center items-center">
+        <button className="bg-[#00BCD4] my-2 px-3 py-2 text-white rounded-xl ">
+          read more
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,20 +2,53 @@ import { useLoaderData } from "react-router-dom";
 import EstateCard from "../Components/EstateCard";
 import Slider from "../Components/Slider/Slider";
 import { Helmet } from "react-helmet";
+import { useEffect, useState } from "react";
+import Services from "../Components/Services";
 
 const Home = () => {
   const estates = useLoaderData();
   // console.log(estates);
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("/services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
   return (
     <div>
       <Helmet>
-      <title>HavenlyHomes | Home</title>
+        <title>HavenlyHomes | Home</title>
       </Helmet>
       <Slider></Slider>
+      <div className="pt-20 flex-col flex justify-center items-center space-y-2">
+        <h1 className="text-4xl font-semibold">
+          Explore Our Estates Collection
+        </h1>
+        <p className="text-lg text-gray-500">
+          Find the epitome of elegance and sophistication among our diverse
+          range of estates.
+        </p>
+      </div>
       <div className="grid grid-cols-1 gap-y-10 md:grid-cols-2 lg:grid-cols-3 py-10 w-full">
         {estates.map((estate) => (
           <EstateCard key={estate.id} estate={estate}></EstateCard>
         ))}
+      </div>
+      <div className="py-20">
+        <div className="flex justify-center items-center flex-col space-y-4 ">
+          <h1 className="text-5xl font-semibold">Our Services for You</h1>
+          <p className="max-w-2xl text-center text-gray-500">
+            Explore a range of professional services designed to streamline your
+            real estate transactions. Whether you're buying, selling, or
+            renting, our team is dedicated to delivering exceptional results and
+            exceeding your expectations.
+          </p>
+        </div>
+        <div className="flex justify-between items-center pt-10">
+          {services.map((service, id) => (
+            <Services key={id} service={service}></Services>
+          ))}
+        </div>
       </div>
     </div>
   );
